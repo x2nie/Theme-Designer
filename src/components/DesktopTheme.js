45e111d2818188624d37464ef5a13d355be7da45
Fathony L <fathony@smart-leaders.net>
2024-12-15 11:30:31 +0700

    preview = wow. +hittest

--- src/components/DesktopTheme.js
+++ src/components/DesktopTheme.js
@@ -1,7 +1,21 @@
 import { Component } from "@odoo/owl";
 
 import './DesktopTheme.scss'
+import './PreviewBox.scss'
 
 export class DesktopTheme extends Component{
     static template = "DesktopTheme.Form"
+
+
+    previewClick(ev){
+        console.log('EL:', findEl(ev.target))
+    }
+}
+
+function findEl(el) {
+    var name = el.getAttribute('data-el');
+    if(name) return name;
+    if(el.parentElement){
+        return findEl(el.parentElement)
+    }
 }
